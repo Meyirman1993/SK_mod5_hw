5d3e05e1f01d108b2914718c34a2e9da4333b1d5
Meyirman Turlybekov <turlibekov.m.m@gmail.com>
2022-03-26 00:22:41 +0600

    rename main file

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -28,8 +28,40 @@ class _MainPageWidgetState extends State<MainPageWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
-      floatingActionButton: const _FloatingActionButtomBottomSheet(),
+      floatingActionButton: Builder(
+        builder: (BuildContext context) {
+          return FloatingActionButton.extended(
+            onPressed: () {
+              Scaffold.of(context).showBottomSheet<void>(
+                  (BuildContext context) => Container(
+                        width: double.infinity,
+                        height: 200,
+                        color: Colors.amber,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              const Text('data'),
+                              const Text('data'),
+                              const Text('data'),
+                              ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text('Exit'))
+                            ],
+                          ),
+                        ),
+                      ),
+                  clipBehavior: Clip.antiAlias);
+            },
+            label: const Text('Some Text'),
+            icon: const Icon(Icons.abc_outlined),
+          );
+        },
+      ),
       drawer: const _DrawerLeft(),
       endDrawer: const _EndDrawerUserInfo(),
       appBar: AppBar(
@@ -57,65 +89,6 @@ class _MainPageWidgetState extends State<MainPageWidget> {
   }
 }
 
-class _FloatingActionButtomBottomSheet extends StatelessWidget {
-  const _FloatingActionButtomBottomSheet({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return FloatingActionButton.extended(
-      onPressed: () {
-        showModalBottomSheet(
-          context: context,
-          builder: (_) {
-            return const _ModalBottomSheet();
-          },
-        );
-      },
-      label: const Text('Open Modal Bottom Sheet.'),
-    );
-  }
-}
-
-class _ModalBottomSheet extends StatefulWidget {
-  const _ModalBottomSheet({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  State<_ModalBottomSheet> createState() => _ModalBottomSheetState();
-}
-
-class _ModalBottomSheetState extends State<_ModalBottomSheet> {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.only(top: 20),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text(
-              'Bottom Sheet',
-              style: TextStyle(fontSize: 30),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(25),
-              child: OutlinedButton(
-                child: const Text('Back'),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
 class _EndDrawerUserInfo extends StatelessWidget {
   const _EndDrawerUserInfo({
     Key? key,
@@ -135,7 +108,6 @@ class _EndDrawerUserInfo extends StatelessWidget {
             backgroundColor: Color(0xFFE53935),
           ),
         ),
-        // SizedBox(height: 20),
         Text('Eminem Marshall')
       ],
     ));
@@ -153,6 +125,12 @@ class _EndDrawerOpenKey extends StatelessWidget {
       builder: (context) => IconButton(
         onPressed: () {
           Scaffold.of(context).openEndDrawer();
+          showModalBottomSheet(
+              isScrollControlled: true,
+              context: context,
+              builder: (BuildContext context) {
+                return Column();
+              });
         },
         icon: const Icon(Icons.person_outlined),
       ),
